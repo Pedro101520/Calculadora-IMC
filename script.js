d24c95de5766027a27c51e9f117f6c11528acb7a
Pedro101520 <pedrolimazoi10@gmail.com>
2022-08-12 11:54:21 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const calcular = document.getElementById('calcular');
+
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+        resultado.textContent = valorIMC;
+
+        let classificacao = "";
+        if (valorIMC < 18.5) {
+            classificacao = "Abaixo do peso";
+        } else if (valorIMC < 25){
+            classificacao = "Pesso ideal";
+        }else if (valorIMC < 30){
+            classificacao = "Você está acima do peso";
+        }else if (valorIMC < 35){
+            classificacao = "Você está com obesidade grau 1";
+        }else if (valorIMC < 40){
+            classificacao = "Você está com obesidade de grau 2";
+        }else{
+            classificacao = "Você está com obesidade de grau 3";
+        }
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+    } else {
+        resultado.textContent = 'Preencha todos os campos';
+    }
+}
+
+calcular.addEventListener('click', imc);
